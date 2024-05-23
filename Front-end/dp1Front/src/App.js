098e2380ff42cc6db5890b4f889624f5ae145742
brunomeza1999 <114270410+brunomeza1999@users.noreply.github.com>
2024-05-22 19:57:57 -0500

    Prueba
    Prueba

--- Front-end/dp1Front/src/App.js
+++ Front-end/dp1Front/src/App.js
@@ -85,6 +85,14 @@ function App() {
   const handleCloseReportesPopup = () => {
     setIsReportesPopupOpen(false);
   };
+  
+  
+  
+  
+  
+  
+  
+  
 
   const handleOpenSimulacionSidebar = () => {
     setIsSimulacionSidebarOpen(true);
