d99b1b61036d8570a538d2c69e9c29f1480992e1
zaicket <vargas.pedro@pucp.edu.pe>
2024-05-27 15:06:21 -0500

    Services PackageFlight

---
+++ Back-end/dp1_back/src/main/java/com/edu/pucp/dp1/redex/controller/PackageFlightController.java
@@ -0,0 +1,52 @@
+package com.edu.pucp.dp1.redex.controller;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.edu.pucp.dp1.redex.model.PackageFlight;
+import com.edu.pucp.dp1.redex.services.PackageFlightService;
+
+@RestController
+@RequestMapping("/packageFlight")
+@CrossOrigin
+public class PackageFlightController {
+    
+    @Autowired
+    private PackageFlightService packageFlightService;
+
+    @GetMapping(value = "/")
+    List<PackageFlight> getAll(){
+        return packageFlightService.getAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    PackageFlight get(@PathVariable int id){
+        return packageFlightService.get(id);
+    }
+
+    @PostMapping(value = "/")
+    PackageFlight register(@RequestBody PackageFlight packageFlight) throws SQLException{
+        return packageFlightService.register(packageFlight);
+    }
+
+    @PutMapping(value = "/")
+    PackageFlight update(@RequestBody PackageFlight packageFlight) throws SQLException{
+        return packageFlightService.update(packageFlight);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    void delete(@PathVariable int id){
+        packageFlightService.delete(id);
+    }
+}
---
+++ Back-end/dp1_back/src/main/java/com/edu/pucp/dp1/redex/services/PackageFlightService.java
@@ -0,0 +1,64 @@
+package com.edu.pucp.dp1.redex.services;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.edu.pucp.dp1.redex.model.PackageFlight;
+import com.edu.pucp.dp1.redex.repository.PackageFlightRepository;
+
+@Service
+public class PackageFlightService {
+    
+    @Autowired
+    private PackageFlightRepository packageFlightRepository;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PackageFlightService.class);
+
+    public PackageFlight register(PackageFlight packageFlight){
+        try {
+            return packageFlightRepository.save(packageFlight);
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            return null;
+        }
+    }
+
+    public List<PackageFlight> getAll(){
+        try {
+            return packageFlightRepository.findAll();
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            return null;
+        }
+    }
+
+    public PackageFlight get(int id){
+        try {
+            return packageFlightRepository.findPackageFlightById(id);
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            return null;
+        }
+    }
+
+    public PackageFlight update(PackageFlight packageFlight){
+        try {
+            return packageFlightRepository.save(packageFlight);
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            return null;
+        }
+    }
+
+    public void delete(int id){
+        try {
+            packageFlightRepository.deleteById(id);
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+        }
+    }
+}
