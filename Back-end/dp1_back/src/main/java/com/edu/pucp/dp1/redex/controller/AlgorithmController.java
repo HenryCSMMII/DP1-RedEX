8ed7edcdf5452e798a2f069a98b0c0a9a0fedf70
Eduardo Izaguirre Pastor <89656213+eizag@users.noreply.github.com>
2024-06-02 01:37:47 -0500

    Update AlgoritmoController

--- Back-end/dp1_back/src/main/java/com/edu/pucp/dp1/redex/controller/AlgorithmController.java
+++ Back-end/dp1_back/src/main/java/com/edu/pucp/dp1/redex/controller/AlgorithmController.java
@@ -6,6 +6,7 @@ import com.edu.pucp.dp1.redex.dto.FlightDTO;
 import com.edu.pucp.dp1.redex.model.Airport;
 import com.edu.pucp.dp1.redex.model.Flight;
 import com.edu.pucp.dp1.redex.model.Algorithm.BD;
+import com.edu.pucp.dp1.redex.model.Algorithm.CalendarFlights;
 import com.edu.pucp.dp1.redex.model.Algorithm.Individual;
 import com.edu.pucp.dp1.redex.model.Algorithm.Population;
 import com.edu.pucp.dp1.redex.services.AlgorithmService;
@@ -15,6 +16,7 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -61,125 +63,112 @@ public class AlgorithmController {
 	
 	@CrossOrigin
 	@RequestMapping(value="run/", method = RequestMethod.GET)
-	public List<Flight> genetic_algorithm(long date_simulation, int type_simulation){
+	public List<Flight> genetic_algorithm(long dateSim, int typeSim){
 		
 		//FileReader.read_list_airports();
 		//FileReader.read_list_flights();
-		FileReader.read_list_shipment_with_date(date_simulation, type_simulation);
-		CalendarFlightPool.generate_calendar();
+		FileReader.read_list_shipment_with_date(dateSim, typeSim);
+		CalendarFlights.generate_calendar();
 		
-		Population population = new Population(geneticParameters.POPULATION_NUM_INDIVIDUALS);
+		int iterGen=0;
+		Population population = new Population(10); //cant de individuos
 		
 		population.initialize(BD.list_shipment);
 		population.evaluate();
-
-		int num_generations=0;
-		int index_best_individual = 0;
 		
-		while(num_generations != geneticParameters.NUM_GENERATIONS) {
+		while(iterGen != 10) { //cant de individuos
 		
-			List<Individual[]> new_parents = new ArrayList<Individual[]>();
+			List<Individual[]> newParents = new ArrayList<Individual[]>();
 			
-			//System.out.println("============== HOLA ==============");
-			
-			for(int i = 0; i < geneticParameters.POPULATION_NUM_INDIVIDUALS/2; i++) {
-				//System.out.println("============== ROULETTE ==============");
+			for(int i = 0; i < 5; i++) { //cant de individuos % 2
 				
-				//Individual[] new_parents_group = population.selection_parents_tournament(population, 4);
-				Individual[] new_parents_group = population.selection_parents_roulette(population);
-				new_parents.add(new_parents_group);
+				//Individual[] newParent = population.selectionTournament(population,4);
+				Individual[] newParent = population.selectionRoulette(population);
+				newParents.add(newParent);
 			}
 			
-			List<Individual> new_offspring = new ArrayList<Individual>();
-			for(int i=0;i<new_parents.size();i++) {
-				//System.out.println("============== CROSSOVER ==============");
-				Individual[] children = new_parents.get(i)[0].crossover_uniform(new_parents.get(i)[1]);
-				new_offspring.add(children[0]);
-				new_offspring.add(children[1]);
+			List<Individual> newOffspring = new ArrayList<Individual>();
+			for(int i=0;i<newParents.size();i++) {
+				Individual[] children = newParents.get(i)[0].crossover_uniform(newParents.get(i)[1]);
+				newOffspring.add(children[0]);
+				newOffspring.add(children[1]);
 			}
 			
-			for(int i=0;i<new_offspring.size();i++) {
-			//	//System.out.println("============== MUTATION 1 ==============");
-				new_offspring.get(i).mutation1(population);
+			for(int i=0;i<newOffspring.size();i++) {
+				newOffspring.get(i).mutation1(population);
 			}
 			
-			Population population_temp = new Population(geneticParameters.POPULATION_NUM_INDIVIDUALS*2);
+			Population tempPopulation = new Population(20); //cant de individuos * 2
 			
-			for(int i=0, k=0;i<new_parents.size();i++, k+=2) {
+			for(int i=0, k=0;i<newParents.size();i++, k+=2) {
 				System.out.println("i :" + i + " k: " + k);
-				population_temp.getIndividuals()[k] = new_parents.get(i)[0];
-				population_temp.getIndividuals()[k+1] = new_parents.get(i)[1];
+				tempPopulation.getIndividuals()[k] = newParents.get(i)[0];
+				tempPopulation.getIndividuals()[k+1] = newParents.get(i)[1];
 			}
 			
-			for(int i=0;i<new_offspring.size();i++) {
-				population_temp.getIndividuals()[i+geneticParameters.POPULATION_NUM_INDIVIDUALS] = new_offspring.get(i);
+			for(int i=0;i<newOffspring.size();i++) {
+				tempPopulation.getIndividuals()[i+10] = newOffspring.get(i); //cant de individuos
 			}
-			
-			//Seleccionar los 10 mejores
-			double[] list_fitness = population_temp.evaluate();
-			
-			System.out.println("==========================");
-			for(int i=0;i<list_fitness.length;i++) {
-				System.out.println(list_fitness[i]);
-			}
-			System.out.println("==========================");
-			
-			
-			double[] list_fitness_temp = Arrays.copyOf(list_fitness, list_fitness.length);
 
-			Arrays.sort(list_fitness_temp);
+			double[] fitness = tempPopulation.evaluate();
 			
-			int[] list_index = new int[geneticParameters.POPULATION_NUM_INDIVIDUALS];
+			for(int i=0;i<fitness.length;i++) {
+				System.out.println(fitness[i]);
+			}
 			
+			double[] fitnessTemp = Arrays.copyOf(fitness, fitness.length);
+			Arrays.sort(fitnessTemp);
+			int[] listIndex = new int[10]; //cant de individuos
 			int j=0;
-			int flag_exist=0;
-			for(int i=list_fitness_temp.length-1;i>=0;i--) {
-				if(i ==  list_fitness_temp.length-1-geneticParameters.POPULATION_NUM_INDIVIDUALS) break;
-				flag_exist = 0;
-				for(int k=0;k<list_fitness.length;k++) {
-					if(list_fitness[k] == list_fitness_temp[i]) {
-						for(int m=0;m<list_index.length;m++) {
-							if(list_index[m] == k) {
-								flag_exist = 1; //significa que este indice ya existe uwu
+			int flag=0;
+
+			for(int i = fitnessTemp.length-1; i>=0; i--) {
+
+				if(i == fitnessTemp.length-1-10) break; //cant de individuos
+				flag = 0;
+
+				for(int k=0;k<fitness.length;k++) {
+
+					if(fitness[k] == fitnessTemp[i]) {
+						for(int l=0;l<listIndex.length;l++) {
+							if(listIndex[l] == k) {
+								flag = 1;
 							}							
 						}
-						if(flag_exist == 1) {
-							flag_exist = 0;
+						
+						if(flag == 1) {
+							flag = 0;
 							continue;
 						}
-						list_index[j] = k;
+						listIndex[j] = k;
 						break;
 					}
 				}
-				j+=1;
+				j++;
 			}
 			
 			//Population population_selected = new Population(geneticParameters.POPULATION_NUM_INDIVIDUALS);
 			//for(int i=0;i<10;i++) {
-			//	population_selected.getIndividuals() = population_temp.getIndividuals()
+			//	population_selected.getIndividuals() = tempPopulation.getIndividuals()
 			//}
 
-			population = new Population(geneticParameters.POPULATION_NUM_INDIVIDUALS);
-			for(int i=0;i<geneticParameters.POPULATION_NUM_INDIVIDUALS;i++) {
-				population.getIndividuals()[i] = population_temp.getIndividuals()[list_index[i]];
+			population = new Population(10); //cant de individuos
+			for(int i=0;i<10;i++) { //cant de individuos
+				population.getIndividuals()[i] = tempPopulation.getIndividuals()[listIndex[i]];
 			}
 	
 			for(int k=0;k<2;k++) {
-				for(int n=0;n<=365;n++) {
-					for(int m=0;m<BD.list_pool_fligths[k][n].size();m++) {
-						for(int a=0;a<BD.list_pool_fligths[k][n].get(m).getUsed_capacity().length;a++) {
-							if(a==geneticParameters.POPULATION_NUM_INDIVIDUALS) break;
-							BD.list_pool_fligths[k][n].get(m).getUsed_capacity()[a]= 
-                                BD.list_pool_fligths[k][n].get(m).getUsed_capacity()[list_index[a]];
+				for(int l=0;l<=365;l++) {
+					for(int m=0;m<BD.list_pool_fligths[k][l].size();m++) {
+						for(int n=0;n<BD.list_pool_fligths[k][l].get(m).getCurrentLoad().length;n++) {
+							if(n==10) break; //cant de individuos
+							BD.list_pool_fligths[k][l].get(m).getCurrentLoad()[n]= 
+                                BD.list_pool_fligths[k][l].get(m).getCurrentLoad()[listIndex[n]];
 						}
 					}
 				}
 			}
-
-			//los guardo en una nueva poblacion // los 10 mejores descendientes
- 
-			num_generations+=1;
-
+			iterGen+=1;
 		}
 		/*
 		int num=0;
@@ -193,18 +182,18 @@ public class AlgorithmController {
 			}
 		}*/
 		
-		Individual best_individual = population.getIndividuals()[0];
+		Individual bestIndividual = population.getIndividuals()[0];
 		
-		if(best_individual.getFitness(0) < 0.01) {
-			System.out.println("No hay solucion");
+		if(bestIndividual.getFitness(0) < 0.01) {
+			System.out.println("No se ha encontrado un plan de vuelos");
 			return null;
 		}
 		
 		System.out.println("Total de envios sin solucion "+ BD.list_shipments_without_solution.size());
 		for (int i = 0; i < BD.list_shipments_without_solution.size(); i++) {
-			if(BD.list_shipments_without_solution.get(i).getPackageQuantity() >= 25) {
-				System.out.println("SALIDA: " + BD.list_shipments_without_solution.get(i).getDepartureAirport().getCode());
-				System.out.println("LLEGADA: " + BD.list_shipments_without_solution.get(i).getArrivalAirport().getCode());
+			if(BD.list_shipments_without_solution.get(i).getCantidad() >= 25) {
+				System.out.println("SALIDA: " + BD.list_shipments_without_solution.get(i).getOrigen().getCodigoIATA());
+				System.out.println("LLEGADA: " + BD.list_shipments_without_solution.get(i).getDestino().getCodigoIATA());
 			}
 		}
 		
@@ -219,34 +208,29 @@ public class AlgorithmController {
 		//return GeneralData.list_pool_fligths[0][292];
 		
 		/********************************* DAYS OF THE YEAR - ARRAY ***************************************/
-		DateFormat formater_date = new SimpleDateFormat("yyyy-MM-dd");
-		LocalDate date_array = LocalDate.parse(formater_date.format(new Date(date_simulation)));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate arrDate = LocalDate.parse(LocalDate.ofEpochDay(dateSim / (24 * 60 * 60 * 1000)).toString(), formatter);
 				
-		int day_of_year = date_array.getDayOfYear();
-		System.out.println("DIA DEL AÑO: " + (day_of_year));
+		int dd = arrDate.getDayOfYear();
+		System.out.println("DIA DEL AÑO: " + (dd));
 		
-		int year_of_date;
-		System.out.println("AÑO DE LA FECHA: " + date_array.getYear());
+		int yy;
+		System.out.println("AÑO DE LA FECHA: " + arrDate.getYear());
 		
-		if(date_array.getYear() == 2022) { year_of_date = 0; }
-		else { year_of_date = 1; }
+		if(arrDate.getYear() == 2022) { yy = 0; }
+		else { yy = 1; }
 				
-		if(type_simulation == 5) {
-			for (int i = 0; i < type_simulation; i++) {
-				System.out.println("DIA DEL AÑO: " + (day_of_year + i));
+		if(typeSim == 7) { // deberia ser 7 porque es semanal
+			for (int i = 0; i < typeSim; i++) {
 				// FIN DE AÑO -> verificar casos
-				if(day_of_year + i >= 365) {
-					BD.list_pool_fligths[year_of_date][day_of_year - 1].addAll(BD.list_pool_fligths[year_of_date + 1][i]);
+				if(dd + i >= 365) {
+					BD.list_pool_fligths[yy][dd - 1].addAll(BD.list_pool_fligths[yy + 1][i]);
 				}
 				else{
-					BD.list_pool_fligths[year_of_date][day_of_year - 1].addAll(BD.list_pool_fligths[year_of_date][day_of_year + i]);
+					BD.list_pool_fligths[yy][dd - 1].addAll(BD.list_pool_fligths[yy][dd + i]);
 				}
 			}
 		}
-		
-		return BD.list_pool_fligths[year_of_date][day_of_year - 1];
+		return BD.list_pool_fligths[yy][dd - 1];
 	}
-
-
-
 }
