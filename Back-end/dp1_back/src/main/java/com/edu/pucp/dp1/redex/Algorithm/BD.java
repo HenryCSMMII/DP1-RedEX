bce25f699ce76da843eeb930321fdc12b121fa82
HenryCS <henryca8@hotmail.com>
2024-06-03 17:33:54 -0500

    Archivo vuelos actualizado

--- Back-end/dp1_back/src/main/java/com/edu/pucp/dp1/redex/Algorithm/BD.java
+++ Back-end/dp1_back/src/main/java/com/edu/pucp/dp1/redex/Algorithm/BD.java
@@ -35,9 +35,9 @@ public class BD {
 	public static final int POPULATION_NUM_INDIVIDUALS = 10;
 	public static final int NUM_GENERATIONS = 10;
 	
-	public static final int MAX_CAPACITY_FLIGHT_AMERICA = 300;
-	public static final int MAX_CAPACITY_FLIGHT_EUROPE = 250;
-	public static final int MAX_CAPACITY_FLIGHT_TWO_CONTINENTS = 350;
+	// public static final int MAX_CAPACITY_FLIGHT_AMERICA = 300;
+	// public static final int MAX_CAPACITY_FLIGHT_EUROPE = 250;
+	// public static final int MAX_CAPACITY_FLIGHT_TWO_CONTINENTS = 350;
 	
 	// public static final int MAX_CAPACITY_STORAGE_AMERICA = 850;
 	// public static final int MAX_CAPACITY_STORAGE_EUROPE = 900;
