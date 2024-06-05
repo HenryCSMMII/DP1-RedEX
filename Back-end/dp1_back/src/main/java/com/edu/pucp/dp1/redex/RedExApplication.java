73f94690daccc42c758a1eb4272d0d73fa3d9c1b
zaicket <vargas.pedro@pucp.edu.pe>
2024-06-05 00:44:05 -0500

    contrato con il diavlo

--- Back-end/dp1_back/src/main/java/com/edu/pucp/dp1/redex/RedExApplication.java
+++ Back-end/dp1_back/src/main/java/com/edu/pucp/dp1/redex/RedExApplication.java
@@ -3,6 +3,12 @@ package com.edu.pucp.dp1.redex;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+
+
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableCaching
@@ -12,4 +18,16 @@ public class RedExApplication {
 		SpringApplication.run(RedExApplication.class, args);
         System.out.println("Ejecutanding...");
 	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.addAllowedOrigin("*");
+		config.addAllowedMethod("*");
+		config.addAllowedHeader("*");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
+
 }
