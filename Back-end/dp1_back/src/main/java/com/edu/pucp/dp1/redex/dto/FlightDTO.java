9d431c453f134f4bc84734858e5d0622e370489a
Eduardo Izaguirre Pastor <89656213+eizag@users.noreply.github.com>
2024-06-10 16:53:43 -0500

    estadovuelo y estadopaquete

--- Back-end/dp1_back/src/main/java/com/edu/pucp/dp1/redex/dto/FlightDTO.java
+++ Back-end/dp1_back/src/main/java/com/edu/pucp/dp1/redex/dto/FlightDTO.java
@@ -3,7 +3,6 @@ package com.edu.pucp.dp1.redex.dto;
 import java.util.Date;
 import java.util.List;
 
-import com.edu.pucp.dp1.redex.model.Airport;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,33 +14,31 @@ import lombok.Setter;
 @Getter @Setter
 @JsonInclude(JsonInclude.Include.NON_DEFAULT)
 public class FlightDTO {
-	private int id;
-	private String code;
-	private int max_capacity;
-	private int used_capacity;
-	private Date departure_date_time;
-	private Date arrival_date_time;
-	private int departure_airportId;
-	private int arrival_airportId;
-	private long estimated_time;
+    private int id;
+    private String code;
+    private int max_capacity;
+    private int used_capacity;
+    private Date departure_date_time;
+    private Date arrival_date_time;
+    private int departure_airportId;
+    private int arrival_airportId;
+    private long estimated_time;
+    private String state;
 
-	@JsonInclude(JsonInclude.Include.NON_DEFAULT)
-	private List<ShipmentDTO> Shipment=null;
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
+    private List<ShipmentDTO> shipment = null;
 
-	public FlightDTO(int id, String code, int max_capacity, int used_capacity, Date departure_date_time,
-			Date arrival_date_time, int departure_airportId, int arrival_airportId, long estimated_time) {
-		this.id = id;
-		this.code = code;
-		this.max_capacity = max_capacity;
-		this.used_capacity = used_capacity;
-		this.departure_date_time = departure_date_time;
-		this.arrival_date_time = arrival_date_time;
-		this.departure_airportId = departure_airportId;
-		this.arrival_airportId = arrival_airportId;
-		this.estimated_time = estimated_time;
-	}
-
-	
-
-	
+    public FlightDTO(int id, String code, int max_capacity, int used_capacity, Date departure_date_time,
+                     Date arrival_date_time, int departure_airportId, int arrival_airportId, long estimated_time, String state) {
+        this.id = id;
+        this.code = code;
+        this.max_capacity = max_capacity;
+        this.used_capacity = used_capacity;
+        this.departure_date_time = departure_date_time;
+        this.arrival_date_time = arrival_date_time;
+        this.departure_airportId = departure_airportId;
+        this.arrival_airportId = arrival_airportId;
+        this.estimated_time = estimated_time;
+        this.state = state;
+    }
 }
