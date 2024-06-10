b82f8ab2e50a4396e589c5019d5421f9b829e507
Eduardo Izaguirre Pastor <89656213+eizag@users.noreply.github.com>
2024-06-10 17:51:42 -0500

    CityController

---
+++ Back-end/dp1_back/src/main/java/com/edu/pucp/dp1/redex/model/City.java
@@ -0,0 +1,18 @@
+package com.edu.pucp.dp1.redex.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+public class City {
+
+    private int id;
+    private int countryId;
+    private String name;
+
+    // Additional constructors or methods if needed
+}
