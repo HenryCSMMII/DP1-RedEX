42592df59ada70dbcd8e78511fbd7c6553166fff
Eduardo Izaguirre Pastor <89656213+eizag@users.noreply.github.com>
2024-06-12 17:53:10 -0500

    registrar Shipment

--- Back-end/dp1_back/src/main/java/com/edu/pucp/dp1/redex/controller/ShipmentController.java
+++ Back-end/dp1_back/src/main/java/com/edu/pucp/dp1/redex/controller/ShipmentController.java
@@ -1,12 +1,16 @@
 package com.edu.pucp.dp1.redex.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.edu.pucp.dp1.redex.Algorithm.BD;
 import com.edu.pucp.dp1.redex.dto.ShipmentDTO;
+import com.edu.pucp.dp1.redex.model.Airport;
 import com.edu.pucp.dp1.redex.model.Shipment;
+import com.edu.pucp.dp1.redex.model.State;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -40,4 +44,32 @@ public class ShipmentController {
             return "Error al leer el archivo de envíos.";
         }
     }
+
+    @PostMapping("/create")
+    public String createShipment(@RequestBody Shipment request) {
+        try {
+            // Set departure airport
+            Airport departure = BD.airports.stream()
+                    .filter(a -> a.getId() == request.getDepartureAirport().getId())
+                    .findFirst()
+                    .orElse(null);
+            request.setDeparturAirport(departure);
+            
+            // Set arrival airport
+            Airport arrival = BD.airports.stream()
+                    .filter(a -> a.getId() == request.getArrivalAirport().getId())
+                    .findFirst()
+                    .orElse(null);
+            request.setArrivalAirport(arrival);
+            
+            // Set state
+            request.setState(State.Creado);
+            
+            BD.shipmentsTemp.add(request);
+            return "Envío creado exitosamente.";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Error al crear el envío.";
+        }
+    }    
 }
