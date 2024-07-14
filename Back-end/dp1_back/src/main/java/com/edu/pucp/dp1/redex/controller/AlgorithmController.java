63d23711e5473041e897b56aa9ac1cd4c07361c0
zaicket <vargas.pedro@pucp.edu.pe>
2024-07-14 12:17:00 -0500

    Revert "Reapply "fixeados los horarios (mas o menos)""
    This reverts commit 5f3178df95c5a29b9fe4850db4752fa28b0186ae.

--- Back-end/dp1_back/src/main/java/com/edu/pucp/dp1/redex/controller/AlgorithmController.java
+++ Back-end/dp1_back/src/main/java/com/edu/pucp/dp1/redex/controller/AlgorithmController.java
@@ -225,9 +225,6 @@ public List<Flight> genetic_algorithm(@RequestBody YourRequestData requestData){
                 }
             }
             if (noEncontrado) {
-                
-                flight.addHoursToConvertedDateTime();
-
                 BD.flightsResolved.add(flight);
                 BD.flightsResolved.get(vuelos.size()).setShipments(new ArrayList<>());
                 BD.flightsResolved.get(vuelos.size()).getShipments().add(population.getIndividuals()[0].getList_shipments().get(i));
@@ -239,9 +236,6 @@ public List<Flight> genetic_algorithm(@RequestBody YourRequestData requestData){
     }
     System.out.println("ARREGLO VUELOS\n" + vuelos);
     BD.shipmentsTemp = new ArrayList<>();
-
-    
-
     return BD.flightsResolved;
 }
 
